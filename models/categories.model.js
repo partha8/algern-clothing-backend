f3824fa42d7210d78ae3d17d7e5c6087b6aa7dab
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2022-12-21 20:26:13 +0530

    categories schema created + imported dummy data

---
+++ models/categories.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const categoriesSchema = new mongoose.Schema({
+  categoryName: String,
+});
+
+const Categories = mongoose.model("Categories", categoriesSchema);
+export default Categories;
