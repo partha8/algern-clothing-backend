0ba8e64bffaa3447b2169cae87f68cd86a94b4b8
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2022-12-24 13:01:16 +0530

    address endpoint added

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
 const router = Router();
-import pkg from "lodash";
-const { extend } = pkg;
-import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
 
 import dotenv from "dotenv";
 import authVerify from "../middlewares/auth-verify.middleware.js";
@@ -16,6 +12,36 @@ router.route("/").get(authVerify, async (req, res) => {
     const { user } = req;
     const foundUser = await User.findById({ _id: user._id });
     res.status(200).json({ foundUser, encodedToken: user.encodedToken });
+  } catch (error) {
+    console.error(error);
+    return res.status(error.status).send({ message: error.message });
+  }
+});
+
+router.route("/address").post(authVerify, async (req, res) => {
+  try {
+    const { user } = req;
+    const body = req.body;
+
+    const foundUser = await User.findById({ _id: user._id });
+
+    if (body.action.type === "delete") {
+      foundUser.addresses = foundUser.addresses.filter(
+        (address) => address._id != body.address._id
+      );
+    } else if (body.action.type === "edit") {
+      foundUser.addresses = foundUser.addresses.map((address) => {
+        if (address._id == body.address._id) {
+          return { ...body.address };
+        }
+        return address;
+      });
+    } else {
+      foundUser.addresses.push(body.address);
+    }
+
+    let updatedUser = await foundUser.save();
+    res.status(200).json({ foundUser: updatedUser });
   } catch (error) {
     console.error(error);
     return res.status(error.status).send({ message: error.message });
